809f06edbf4f1b491e94e8cd7d67368c19e0690f
geekqq <jinwei1014@gmail.com>
2024-03-06 11:37:43 -0800

    upldate the URL to glowing button

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -27,7 +27,7 @@ const Work = () => {
                             React JS Application
                         </span>
                         <div className='pt-8 text-center'>
-                            <a href='https://icz.one'>
+                            <a href='https://buttondemo-beta.vercel.app/'>
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                                     Demo
                                 </button>
